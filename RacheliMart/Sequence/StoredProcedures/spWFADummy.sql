8928559003d67938e8deac516fb26f3d4048a695
UploadFiles Command <UploadFiles@pnmsoft.com>
2021-06-01 09:11:35 +0300

    UploadFiles

---
+++ RacheliMart/Sequence/StoredProcedures/spWFADummy.sql
@@ -0,0 +1,26 @@
+CREATE OR ALTER  PROCEDURE [dbo].[spwfadummy]
+AS 
+	
+SELECT	200
+
+ GO
+ declare @objectName sysname
+ declare @object_id int
+ declare @datetime_val datetime
+ set @objectName = 'spwfadummy'
+ set @object_id = object_id(@objectName);
+ set @datetime_val = '2021-05-25T09:07:35.274'
+ if exists(select * from sys.extended_properties where name = 'CreationDate' and major_id = @object_id)
+ begin
+ exec sp_updateextendedproperty
+ @level0type = 'SCHEMA', @level0name = 'dbo',
+ @level1type = 'PROCEDURE', @level1name = 'spwfadummy',
+ @name = 'CreationDate', @value = @datetime_val
+ end
+ else
+ begin
+ exec sp_addextendedproperty
+ @level0type = 'SCHEMA', @level0name = 'dbo',
+ @level1type = 'PROCEDURE', @level1name = 'spwfadummy',
+ @name = 'CreationDate', @value = @datetime_val
+ end
