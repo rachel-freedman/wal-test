7f6b20c5ef128fe53ef745bd6ee627355df6824a
rachelf <rachel.freedman@genpact.com>
2021-06-17 14:59:42 +0300

    1234321

--- LATAM-SOE/Sequence/StoredProcedures/spwfa_copycustomdataexmaple.sql
+++ LATAM-SOE/Sequence/StoredProcedures/spwfa_copycustomdataexmaple.sql
@@ -24,7 +24,7 @@ WHERE
  declare @datetime_val datetime
  set @objectName = 'spwfa_copycustomdataexmaple'
  set @object_id = object_id(@objectName);
- set @datetime_val = '2021-06-17T11:48:20.777'
+ set @datetime_val = '2021-06-17T11:59:20.945'
  if exists(select * from sys.extended_properties where name = 'CreationDate' and major_id = @object_id)
  begin
  exec sp_updateextendedproperty
