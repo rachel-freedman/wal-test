d726f0388d41ccdf5f047c436f1c5bbb22f02022
rachelf <rachel.freedman@genpact.com>
2021-06-30 08:07:39 +0300

    2222

---
+++ LATAM-SOE/Sequence/Stored Procedures/CTSTOPMRF.dbo.spwfa_copycustomdataexmaple.sql
@@ -0,0 +1,41 @@
+CREATE OR ALTER     PROCEDURE [dbo].[spwfa_copycustomdataexmaple]
+(
+	@WorkflowInstanceID bigint,
+	@MasterWorkflowInstanceId bigint
+)
+
+AS
+
+
+
+UPDATE UACTcec10eed254f43999e47e76b354587d8
+SET
+	fldMasterIWfId = @MasterWorkflowInstanceId
+WHERE
+	fldIWfId = @WorkflowInstanceID
+
+
+
+
+
+ GO
+ declare @objectName sysname
+ declare @object_id int
+ declare @datetime_val datetime
+ set @objectName = 'spwfa_copycustomdataexmaple'
+ set @object_id = object_id(@objectName);
+ set @datetime_val = '2021-06-30T05:07:18.571'
+ if exists(select * from sys.extended_properties where name = 'CreationDate' and major_id = @object_id)
+ begin
+ exec sp_updateextendedproperty
+ @level0type = 'SCHEMA', @level0name = 'dbo',
+ @level1type = 'PROCEDURE', @level1name = 'spwfa_copycustomdataexmaple',
+ @name = 'CreationDate', @value = @datetime_val
+ end
+ else
+ begin
+ exec sp_addextendedproperty
+ @level0type = 'SCHEMA', @level0name = 'dbo',
+ @level1type = 'PROCEDURE', @level1name = 'spwfa_copycustomdataexmaple',
+ @name = 'CreationDate', @value = @datetime_val
+ end
