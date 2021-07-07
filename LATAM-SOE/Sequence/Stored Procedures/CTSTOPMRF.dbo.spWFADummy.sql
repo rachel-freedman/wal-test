8467dccdcf537ad1d57a80b2a841834910ddb76a
rachelf <rachel.freedman@genpact.com>
2021-07-07 14:05:59 +0300

    123

--- LATAM-SOE/Sequence/Stored Procedures/CTSTOPMRF.dbo.spWFADummy.sql
+++ LATAM-SOE/Sequence/Stored Procedures/CTSTOPMRF.dbo.spWFADummy.sql
@@ -9,7 +9,7 @@ SELECT	1
  declare @datetime_val datetime
  set @objectName = 'spwfadummy'
  set @object_id = object_id(@objectName);
- set @datetime_val = '2021-07-07T11:02:42.356'
+ set @datetime_val = '2021-07-07T11:05:46.310'
  if exists(select * from sys.extended_properties where name = 'CreationDate' and major_id = @object_id)
  begin
  exec sp_updateextendedproperty
